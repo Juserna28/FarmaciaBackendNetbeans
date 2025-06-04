caedfb55688eb83344abb2ed2a2adab2af50a209
Juserna28 <160074975+Juserna28@users.noreply.github.com>
2025-06-03 19:42:49 -0500

    Commit inicial creación

---
+++ FarmaciaPedidos/src/main/java/com/mycompany/farmaciapedidos/FarmaciaPedidos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.farmaciapedidos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class FarmaciaPedidos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
